d885cc2a85b308e91736fb27233b21923128a6a5
z1g99 <maxtkachenkov45@gmail.com>
2024-11-10 18:45:36 +0300

    Merged sort popup and categories into top bar & change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import {Container, Title} from '@/components/shared'
+import {Container, Title, TopBar} from '@/components/shared'
 
 export default function Home() {
   return <>
     <Container className='mt-10'>
       <Title text='Все пиццы' size='lg' className='font-extrabold'/>
     </Container>
+    <TopBar/>
   </>
 }
---
+++ src/components/shared/top-bar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {cn} from '@/lib/utils'
+import {Container} from '@/components/shared/container'
+import {Categories} from '@/components/shared/categories'
+import {SortPopup} from '@/components/shared/sort-popup'
+
+interface Props {
+	className?: string
+}
+
+export const TopBar: React.FC<Props> = ({className}) => {
+	return (
+		<div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
+			<Container className='flex items-center justify-between'>
+				<Categories/>
+				<SortPopup/>
+			</Container>
+		</div>
+	)
+}
+
