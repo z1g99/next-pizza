50c546a1d99dfefe9c046aa7751018adeb40bbf0
z1g99 <maxtkachenkov45@gmail.com>
2024-11-10 21:35:12 +0300

    Add filters & change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Title, TopBar} from '@/components/shared'
+import {Container, Filters, Title, TopBar} from '@/components/shared'
 
 export default function Home() {
   return <>
@@ -6,5 +6,17 @@ export default function Home() {
       <Title text='Все пиццы' size='lg' className='font-extrabold'/>
     </Container>
     <TopBar/>
+    <Container className='pb-14 mt-10'>
+      <div className='flex gap-[60px]'>
+        {/* Фильтрация */}
+        <div className='w-[250px]'>
+          <Filters/>
+        </div>
+        {/* Список товаров */}
+        <div className='flex-1'>
+          <div className='flex flex-col gap-16'>Список товаров</div>
+        </div>
+      </div>
+    </Container>
   </>
 }
---
+++ src/components/shared/filters.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Title} from '@/components/shared'
+import {FilterCheckbox} from '@/components/shared/filter-checkbox'
+
+interface Props {
+	className?: string
+}
+
+export const Filters: React.FC<Props> = ({className}) => {
+	return (
+		<div className={className}>
+			<Title text='Фильтрация' size='sm' className='mb-5 font-bold'/>
+			<div className='flex flex-col gap-4'>
+				<FilterCheckbox text='Можно собирать' value='1'/>
+				<FilterCheckbox text='Новинки' value='2'/>
+			</div>
+		</div>
+	)
+}
+
