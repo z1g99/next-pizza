cf9c95f0ab3983a7bdb1897d558bf49a5b6e7aee
z1g99 <maxtkachenkov45@gmail.com>
2024-11-10 17:39:22 +0300

    Add title & change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import {Container, Title} from '@/components/shared'
+
 export default function Home() {
-  return (
-    <div></div>
-  );
+  return <>
+    <Container className='mt-10'>
+      <Title text='Все пиццы' size='lg' className='font-extrabold'/>
+    </Container>
+  </>
 }
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,2 +1,3 @@
 export { Header } from './header'
 export { Container } from './container'
+export { Title } from './title'
