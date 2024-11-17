94cf0d9f3df46b4cf941680341d775d707e419f4
z1g99 <maxtkachenkov45@gmail.com>
2024-11-17 17:11:42 +0300

    Add seed (product variant)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,14 @@ import {prisma} from './prisma-client'
 import {hashSync} from 'bcrypt'
 import {categories, ingredients, products} from './constants'
 
+const randomNumber = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+const price = (pizza: boolean)=> {
+	return pizza ? randomNumber(150, 500) : randomNumber(50, 250)
+}
+
 async function up() {
 	await prisma.user.createMany({
 		data: [
@@ -69,10 +77,55 @@ async function up() {
 			},
 		},
 	})
+	
+	await prisma.productVariant.createMany({
+		data: [
+			// Пицца "Пепперони фреш"
+			{ productId: pizza1.id, pizzaType: 'THIN', size: 'SMALL', price: price(true) },
+			{ productId: pizza1.id, pizzaType: 'REGULAR', size: 'MEDIUM', price: price(true) },
+			{ productId: pizza1.id, pizzaType: 'REGULAR', size: 'LARGE', price: price(true) },
+			
+			// Пицца "Сырная"
+			{ productId: pizza2.id, pizzaType: 'THIN', size: 'SMALL', price: price(true) },
+			{ productId: pizza2.id, pizzaType: 'THIN', size: 'MEDIUM', price: price(true) },
+			{ productId: pizza2.id, pizzaType: 'THIN', size: 'LARGE', price: price(true) },
+			{ productId: pizza2.id, pizzaType: 'REGULAR', size: 'SMALL', price: price(true) },
+			{ productId: pizza2.id, pizzaType: 'REGULAR', size: 'MEDIUM', price: price(true) },
+			{ productId: pizza2.id, pizzaType: 'REGULAR', size: 'LARGE', price: price(true) },
+			
+			// Пицца "Чоризо фреш"
+			{ productId: pizza3.id, pizzaType: 'THIN', size: 'SMALL', price: price(true) },
+			{ productId: pizza3.id, pizzaType: 'REGULAR', size: 'MEDIUM', price: price(true) },
+			{ productId: pizza3.id, pizzaType: 'REGULAR', size: 'LARGE', price: price(true) },
+			
+			// Остальные продукты
+			{ productId: 1, price: price(false) },
+			{ productId: 2, price: price(false) },
+			{ productId: 3, price: price(false) },
+			{ productId: 4, price: price(false) },
+			{ productId: 5, price: price(false) },
+			{ productId: 6, price: price(false) },
+			{ productId: 7, price: price(false) },
+			{ productId: 8, price: price(false) },
+			{ productId: 9, price: price(false) },
+			{ productId: 10, price: price(false) },
+			{ productId: 11, price: price(false) },
+			{ productId: 12, price: price(false) },
+			{ productId: 13, price: price(false) },
+			{ productId: 14, price: price(false) },
+			{ productId: 15, price: price(false) },
+			{ productId: 16, price: price(false) },
+			{ productId: 17, price: price(false) },
+		]
+	})
 }
 
 async function down() {
 	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "ProductVariant" RESTART IDENTITY CASCADE`
+	await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`
 }
 
 async function main() {
