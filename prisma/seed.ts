a554d42c4b8f642a09a29503ccf67ce69ef6eb37
z1g99 <maxtkachenkov45@gmail.com>
2024-11-17 14:58:41 +0300

    Add seeds (category, ingredient, product & 3 pizzas) & add constants.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import {prisma} from './prisma-client'
 import {hashSync} from 'bcrypt'
+import {categories, ingredients, products} from './constants'
 
 async function up() {
 	await prisma.user.createMany({
@@ -19,6 +20,54 @@ async function up() {
 				role: 'ADMIN'
 			},
 		]
+	})
+	
+	await prisma.category.createMany({
+		data: categories
+	})
+	
+	await prisma.ingredient.createMany({
+		data: ingredients
+	})
+	
+	await prisma.product.createMany({
+		data: products
+	})
+	
+	const pizza1 = await prisma.product.create({
+		data: {
+			name: 'Пепперони фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(0, 5),
+			},
+		},
+	})
+	
+	const pizza2 = await prisma.product.create({
+		data: {
+			name: 'Сырная',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(5, 10),
+			},
+		},
+	})
+	
+	const pizza3 = await prisma.product.create({
+		data: {
+			name: 'Чоризо фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(10, 40),
+			},
+		},
 	})
 }
 
